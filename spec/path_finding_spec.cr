2e31eb977973319a2f87a1dfd2f6c2d8ce97cc3c
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-04 09:17:55 +0100

    [GREEN] Change 1

---
+++ spec/path_finding_spec.cr
@@ -0,0 +1,23 @@
+require "./spec_helper"
+require "../src/path_finding"
+
+describe PathFinding do
+  # Since we want to focus on algorithm, lets represent our graph as a
+  # Hash(Int32, Int32) for simplicity's sake. And start and finish points are
+  # Int32 too.
+  #
+  # We will start from simple function, that will tell us, if it is possible
+  # to reach from start to finish, and if it is returns any path.
+  #
+  # Then the signature would look like:
+  # def self.find_path(graph : Hash(Int32, Int32), start : Int32, finish : Int32)
+
+  describe "#find_path" do
+    empty_graph = {} of Int32 => Int32
+    empty_path = [] of Int32
+
+    it "finds any path" do
+      PathFinding.find_path(empty_graph, 0, 0).should eq({false, empty_path})
+    end
+  end
+end
---
+++ src/path_finding.cr
@@ -0,0 +1,9 @@
+module PathFinding
+  def self.find_path(graph, start, finish)
+    {false, empty_path}
+  end
+
+  private def self.empty_path
+    [] of Int32
+  end
+end
