12e57ed33140385bf444033ae3f5f93520e4c691
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-04 09:23:14 +0100

    [RED] Change 13

--- src/path_finding.cr
+++ src/path_finding.cr
@@ -1,12 +1,19 @@
 module PathFinding
   def self.find_path(graph, start, finish)
     return {true, [start]} if start == finish
-    return {true, [start, finish]} if graph[start].includes?(finish)
+
+    if graph.fetch(start, noedges).first == finish
+      return {true, [start, finish]}
+    end
 
     {false, empty_path}
   end
 
   private def self.empty_path
+    [] of Int32
+  end
+
+  private def self.noedges
     [] of Int32
   end
 end
