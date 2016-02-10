e6090616ed34fa935f9bb30b1b8c7ae2cc741bb6
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-10 15:24:10 +0100

    [GREEN] Change 122

--- spec/sort_spec.cr
+++ spec/sort_spec.cr
@@ -20,6 +20,7 @@ describe Sort do
     Sort.sort([2, 1, 3]).should eq([1, 2, 3])
     Sort.sort([2, 3, 1]).should eq([1, 2, 3])
     Sort.sort([3, 2, 1]).should eq([1, 2, 3])
+    Sort.sort([3, 1, 2]).should eq([1, 2, 3])
   end
 
   it "does not modify original array" do
