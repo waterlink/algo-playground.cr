847d64b8bfced0f0f5239a139dcedf63d3e32229
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-10 15:24:01 +0100

    [GREEN] Change 121

--- spec/sort_spec.cr
+++ spec/sort_spec.cr
@@ -16,8 +16,9 @@ describe Sort do
     Sort.sort([2, 1]).should eq([1, 2])
 
     Sort.sort([1, 2, 3]).should eq([1, 2, 3])
-    Sort.sort([2, 1, 3]).should eq([1, 2, 3])
     Sort.sort([1, 3, 2]).should eq([1, 2, 3])
+    Sort.sort([2, 1, 3]).should eq([1, 2, 3])
+    Sort.sort([2, 3, 1]).should eq([1, 2, 3])
     Sort.sort([3, 2, 1]).should eq([1, 2, 3])
   end
 
