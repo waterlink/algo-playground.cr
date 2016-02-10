66c042589c95d1ec1dcc5a84320d14366d37c569
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-10 16:16:11 +0100

    [RED] Change 169

--- src/sort.cr
+++ src/sort.cr
@@ -9,42 +9,6 @@ module Sort
 
   private def sort!(a, l, r, bench)
     return unless l < r - 1
-
-    p = a[l]
-
-    h = l
-    h2 = r - 1
-
-    if h < h2
-      if bigger(a, p, h, bench)
-        swap(a, h, h2, bench)
-        h2 -= 1
-      else
-        h += 1
-      end
-    end
-
-
-    if h < h2
-      if bigger(a, p, h, bench)
-        swap(a, h, h2, bench)
-        h2 -= 1
-      else
-        h += 1
-      end
-    end
-
-
-    if h < h2
-      if bigger(a, p, h, bench)
-        swap(a, h, h2, bench)
-        h2 -= 1
-      else
-        h += 1
-      end
-    end
-
-    sort!(a, l + 1, r, bench)
   end
 
   private def empty
